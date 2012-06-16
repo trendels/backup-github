21c5b44a4f92faae93d06136a326854d00a123ad
trendels <stanis.trendelenburg@gmail.com>
2012-06-16 16:34:01 +0200

    Updated to github api v3 (v2 api has been removed as of 2012-06-12)

--- backup-github.py
+++ backup-github.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import sys
 import urllib2
-import xml.dom.minidom
 import os
 import os.path
 from pipes import quote
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 if len(sys.argv) != 3:
     print "Usage: %s <username> <directory>" % sys.argv[0]
@@ -15,20 +19,18 @@ if not os.path.isdir(backupdir):
     print "Error: '%s' does not exist or is not a directory" % backupdir
     sys.exit(3)
 
-f = urllib2.urlopen('http://github.com/api/v2/xml/repos/show/%s' % username)
-doc = xml.dom.minidom.parse(f)
+f = urllib2.urlopen('https://api.github.com/users/%s/repos' % username)
+repos = json.load(f)
 
-for node in doc.getElementsByTagName('repository'):
-    reponame = node.getElementsByTagName('name')[0].childNodes[0].data
-    url = 'git://github.com/%s/%s.git' % (username, reponame)
-    dir = os.path.join(backupdir, os.path.basename(reponame))
+for repo in repos:
+    dir = os.path.join(backupdir, os.path.basename(repo['name']))
     cwd = os.getcwd()
     if not os.path.exists(dir):
-        print "adding new repository: %s" % url
+        print "adding new repository: %s" % repo['git_url']
         os.mkdir(dir)
         os.chdir(dir)
         os.system('git --bare init')
-        os.system('git remote add --mirror origin %s' % quote(url))
+        os.system('git remote add --mirror origin %s' % quote(repo['git_url']))
     else:
         os.chdir(dir)
 
